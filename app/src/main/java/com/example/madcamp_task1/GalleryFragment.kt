c3961fcda0ab531338ca14414cf5bf78aa2e933c
lucy1287 <lucy1287@naver.com>
2024-06-28 10:55:37 +0900

    Feat: 탭2 이벤트 recyclerview, dialog

---
+++ app/src/main/java/com/example/madcamp_task1/GalleryFragment.kt
@@ -0,0 +1,66 @@
+package com.example.madcamp_task1
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.madcamp_task1.adapter.EventAdapter
+import com.example.madcamp_task1.data.Event
+import com.example.madcamp_task1.databinding.FragmentGalleryBinding
+
+
+class GalleryFragment : Fragment () {
+
+    private lateinit var binding: FragmentGalleryBinding
+
+    private val events = listOf(
+        Event("이탈리아 여행", "와인 투어와 중세 도시 여행", "ddd"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd")
+    );
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGalleryBinding.inflate(layoutInflater)
+        initializeViews()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNewEvent.setOnClickListener {
+
+            val constraintLayout = View.inflate(requireActivity(), R.layout.dialog_new_event_title, null) as ConstraintLayout
+
+            AlertDialog.Builder(requireActivity())
+                .setView(constraintLayout)
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, whichButton ->
+                    val etNewEventName = constraintLayout.findViewById<View>(R.id.et_new_event_title) as EditText
+                    val value = etNewEventName.text.toString()
+                    Log.d("이름", value)
+                    dialog.dismiss()
+
+
+
+                })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener { dialog, whichButton -> dialog.dismiss() })
+                .show()
+        }
+    }
+
+    private fun initializeViews(){
+        binding.rvEventList.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvEventList.adapter = EventAdapter(events)
+
+    }
+}
--- app/src/main/java/com/example/madcamp_task1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_task1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.madcamp_task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.madcamp_task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (findViewById<View>(R.id.fragment_gallery) != null) {
+            if (savedInstanceState != null) {
+                return
+            }
+
+            val galleryFragment = GalleryFragment()
+
+            val fragmentManager: FragmentManager = supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.fragment_gallery, galleryFragment)
+            fragmentTransaction.commit()
+        }
     }
 }
