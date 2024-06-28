d860d35d8ab1fb44323aec1b2a8c327fc902aeb8
lucy1287 <lucy1287@naver.com>
2024-06-28 19:38:28 +0900

    Feat: 새 이벤트 추가 시 최신순 정렬

--- app/src/main/java/com/example/madcamp_task1/GalleryFragment.kt
+++ app/src/main/java/com/example/madcamp_task1/GalleryFragment.kt
@@ -14,20 +14,22 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.madcamp_task1.adapter.EventAdapter
 import com.example.madcamp_task1.data.Event
 import com.example.madcamp_task1.databinding.FragmentGalleryBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class GalleryFragment : Fragment () {
 
     private lateinit var binding: FragmentGalleryBinding
 
-    private val events = listOf(
-        Event("이탈리아 여행", "와인 투어와 중세 도시 여행", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd"),
-        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd")
+    private val events = mutableListOf(
+        Event("이탈리아 여행", "와인 투어와 중세 도시 여행", "ddd", "210623151520"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "210623151557"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "221228031630"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "221228061612"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "230218174533"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "230420164709"),
+        Event("생일 파티", "오랜만에 모여서 생일 파티", "ddd", "240518021803")
     );
 
     override fun onCreateView(
@@ -54,7 +56,14 @@ class GalleryFragment : Fragment () {
                     Log.d("이름", value)
                     dialog.dismiss()
 
+                    val currentDate = Date()
+                    val formatter = SimpleDateFormat("yyMMddHHmmss")
+                    val currentDateString = formatter.format(currentDate)
 
+                    events.add(Event(value, "detail", "ddd", currentDateString))
+                    sortEventsByDate()
+                    Log.d("이벤트 리스트", events.toString())
+                    binding.rvEventList.adapter?.notifyDataSetChanged()
 
                 })
                 .setNegativeButton("취소",
@@ -66,6 +75,12 @@ class GalleryFragment : Fragment () {
     private fun initializeViews(){
         binding.rvEventList.layoutManager = LinearLayoutManager(requireContext())
         binding.rvEventList.adapter = EventAdapter(events)
+        sortEventsByDate()
 
+    }
+
+    private fun sortEventsByDate() {
+        val formatter = SimpleDateFormat("yyMMddHHmmss", Locale.getDefault())
+        events.sortByDescending { event -> formatter.parse(event.createdDate) }
     }
 }
