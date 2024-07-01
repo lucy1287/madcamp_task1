5bbefec3ef001913b8aa870e62059f723618ca4f
lucy1287 <lucy1287@naver.com>
2024-07-01 19:50:04 +0900

    Feat: 검색 dialog

--- app/src/main/java/com/example/madcamp_task1/GameEventActivity.kt
+++ app/src/main/java/com/example/madcamp_task1/GameEventActivity.kt
@@ -1,18 +1,25 @@
 package com.example.madcamp_task1
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.madcamp_task1.adapter.SearchAdapter
 import com.example.madcamp_task1.databinding.ActivityGameeventBinding
+import com.example.madcamp_task1.databinding.DialogAddParticipantBinding
 import com.example.madcamp_task1.roomdb.Profile
 import com.example.madcamp_task1.roomdb.ProfileViewModel
 
@@ -23,6 +30,8 @@ class GameEventActivity : AppCompatActivity() {
     private lateinit var selectedDate: String
 
     private lateinit var binding : ActivityGameeventBinding
+    private lateinit var dialogBinding: DialogAddParticipantBinding
+    private lateinit var alertDialog: AlertDialog
     private val profileViewModel: ProfileViewModel by viewModels()
     private lateinit var searchResults : LiveData<List<Profile>>
     private lateinit var searchAdapter: SearchAdapter
@@ -30,6 +39,10 @@ class GameEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameeventBinding.inflate(layoutInflater)
+        dialogBinding = DialogAddParticipantBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         initializeViews()
         initSearchView()
 
@@ -46,11 +59,16 @@ class GameEventActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK, resultIntent)
             finish()
         }
-
-        setContentView(binding.root)
     }
 
     fun initializeViews() {
+
+        searchAdapter = SearchAdapter { profile ->
+            // 새로운 TextView를 동적으로 생성하고 레이아웃에 추가
+            val textView = createProfileTextView(profile.name)
+            binding.linearLayoutProfiles.addView(textView)
+            alertDialog?.dismiss()
+        }
 //        searchResults = profileViewModel.allProfiles
 //
 //        searchResults.observe(viewLifecycleOwner, { searchResultsList ->
@@ -61,20 +79,52 @@ class GameEventActivity : AppCompatActivity() {
 //        searchAdapter = SearchAdapter()
 //        binding.rvSearchResultList.adapter = searchAdapter
 
-        searchAdapter = SearchAdapter()
-        binding.rvSearchResultList.layoutManager = LinearLayoutManager(this)
-        binding.rvSearchResultList.adapter = searchAdapter
+        //searchAdapter = SearchAdapter()
 
-        searchResults = profileViewModel.allProfiles
+        binding.btnAddParticipant.setOnClickListener {
 
-        searchResults.observe(this, { searchResultsList ->
-            searchAdapter.submitList(searchResultsList)
-        })
+            val constraintLayout = dialogBinding.root as ConstraintLayout
+
+            // 부모 뷰가 있는지 확인하고 제거
+            val parent = constraintLayout.parent as? ViewGroup
+            parent?.removeView(constraintLayout)
+
+            alertDialog = AlertDialog.Builder(this)
+                .setView(constraintLayout)
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, whichButton ->
+
+                })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener { dialog, whichButton -> dialog.dismiss() })
+                .show()
+
+            dialogBinding.rvSearchResultList.layoutManager = LinearLayoutManager(this)
+            dialogBinding.rvSearchResultList.adapter = searchAdapter
+
+            searchResults = profileViewModel.allProfiles
+
+            searchResults.observe(this, { searchResultsList ->
+                searchAdapter.submitList(searchResultsList)
+            })
+        }
+    }
+
+    private fun createProfileTextView(profileName: String): TextView {
+        val textView = TextView(this).apply {
+            text = "$profileName  X"
+            setTextColor(ContextCompat.getColor(this@GameEventActivity, android.R.color.black))
+            setBackgroundResource(R.drawable.item_background_event_participant)
+            setPadding(16, 8, 16, 8)
+            setOnClickListener {
+                (this@GameEventActivity.binding.linearLayoutProfiles as ViewGroup).removeView(this)
+            }
+        }
+        return textView
     }
 
     private fun initSearchView() {
-        binding.search.isSubmitButtonEnabled = true
-        binding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        dialogBinding.search.isSubmitButtonEnabled = true
+        dialogBinding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
